2b1bf150b2ac17acf0f04460d0211bdae799a1db
CodeCademmy <rqlmoura@yahoo.com>
2022-01-06 23:47:07 -0800

    Updates

--- trms-back/src/test/java/com/revature/services/EmployeeServiceTest.java
+++ trms-back/src/test/java/com/revature/services/EmployeeServiceTest.java
@@ -1,8 +1,20 @@
 package com.revature.services;
 
+import org.mockito.Mock;
 
-
+import com.revature.data.CommentDAO;
+import com.revature.data.EmployeeDAO;
+import com.revature.data.ReimbursementDAO;
 
 public class EmployeeServiceTest {
-}
 
+	@Mock
+	private CommentDAO cmtDao;
+
+	@Mock
+	private EmployeeDAO empDao;
+
+	@Mock
+	private ReimbursementDAO reimDao;
+
+}
